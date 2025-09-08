7941f6f9bd1d2c262fc7f3ff47260615101bd933
SRT DEV <sahirruiz4@gmail.com>
2025-09-07 20:52:25 -0500

    Agregar recursos de DynamoDB, SQS y S3 para la gestión de tarjetas y transacciones

---
+++ terraform/modules/cards/data.tf
@@ -0,0 +1,64 @@
+
+
+
+resource "aws_dynamodb_table" "card_table" {
+  name           = "card-table"
+  hash_key       = "uuid"
+  range_key      = "createdAt"
+  billing_mode   = "PAY_PER_REQUEST"
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "transaction_table" {
+  name           = "transaction-table"
+  hash_key       = "uuid"
+  range_key      = "createdAt"
+  billing_mode   = "PAY_PER_REQUEST"
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "card_table_error" {
+  name           = "card-table-error"
+  hash_key       = "uuid"
+  range_key      = "createdAt"
+  billing_mode   = "PAY_PER_REQUEST"
+  attribute {
+    name = "uuid"
+    type = "S"
+  }
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "create_request_card" {
+  name = "create-request-card-sqs"
+}
+
+resource "aws_sqs_queue" "error_create_request_card" {
+  name = "error-create-request-card-sqs"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "transactions_report" {
+  bucket = "transactions-report-bucket-${random_id.suffix.hex}"
+  force_destroy = true
+}
